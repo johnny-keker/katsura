c74f13a2efde01cc50f92842e3948d47e9c75d4e
johnny-keker <giomail.iv@gmail.com>
2019-03-20 01:58:53 +0300

    1005 WA 5 task

--- 1005.cpp
+++ 1005.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stdlib.h>
 
-int t_1005() {
-    int num_rocks;
+int main() {
+    int num_rocks, sum1 = 0, sum2 = 0, weight;
     std::cin >> num_rocks;
-    int weights[num_rocks];
+    for (int i = 0; i < num_rocks; i++)
+        std::cin >> weight;
+        if (sum1 <= sum2 || sum2 >= ((sum1 + sum2) / 2))
+            sum1 += weight;
+        else
+            sum2 += weight;
+    std::cout << abs(sum1 - sum2) << std::endl;
     return 0;
 }
